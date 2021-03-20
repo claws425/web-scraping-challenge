573bd4dcf21bfc03445c7372adf1ccb2627f6fca
claws425 <73760859+claws425@users.noreply.github.com>
2021-03-19 20:30:33 -0400

    modified app code

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,19 +1,24 @@
 # import dependencies
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+
+# import pymongo library
+import pymongo
 import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+# client = pymongo.MongoClient(conn)
 
 # create home route and define home function
 @app.route("/")
 def home():
     # Find one record of data from the mongo database
-    mars_info = mongo.db.mars_collection.find_one()
+    mars_info = conn.db.mars_collection.find_one()
 
     # Return template and data
     return render_template("index.html", mars_data=mars_info)
@@ -25,7 +30,7 @@ def scrape():
     mars_data = scrape_mars.scrape()
 
     # insert the mars data in to the collection
-    mongo.db.mars_collection.update({}, mars_data, upsert=True)
+    conn.db.mars_collection.update({}, mars_data, upsert=True)
 
     # go back to the home page
     return redirect("/")
